2f91da2c8bef5aa5cd50c02cad965ede1539469a
OanaIgnat <oignat@umich.edu>
2021-09-01 15:41:03 -0400

    changed key

--- params.py
+++ params.py
@@ -1,2 +1,2 @@
-api_key = 'AIzaSyD_ZAlkFsDD_Ty9u4NsJWtaLsQg-5WJ4cY'
+api_key = 'AIzaSyAcKaTR0mF9EubPfoiwK3x3zWe7Fc4tQcs'
 # (All the video clips can be downloaded from [Github]&#40;https://github.com/OanaIgnat/miniclips&#41;.)
